6c2db37663c7112467e1199a10c688d322506dca
Neeraj-1994 <pradhanneeraj1994@gmail.com>
2021-01-28 19:26:46 +0600

    feat(production): master branch initialized

---
+++ apps/druk-resale/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'druk-resale-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'druk-resale';
+}
