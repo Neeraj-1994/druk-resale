21e44683a8d4af895a128304da383466cf96eb12
Neeraj-1994 <pradhanneeraj1994@gmail.com>
2021-02-04 19:37:46 +0600

    feat(auth): add and configure login component

---
+++ libs/feature/auth/src/lib/misc/validators/auth.validator.ts
@@ -0,0 +1,37 @@
+import { AbstractControl, FormControl, ValidationErrors, ValidatorFn } from '@angular/forms';
+import * as lpn from 'google-libphonenumber';
+
+
+export const phoneNumberValidator: ValidatorFn = (control: FormControl): ValidationErrors | null =>  {
+  const phoneUtil = lpn.PhoneNumberUtil.getInstance();
+  let value = ((control.value && control.value.toString()) || '').trim();
+  try {
+    if (/^00|^[+]/g.test(value)) {
+      value = value.replace('00', '+');
+    }
+    const parsedNumber = value.startsWith('+') || value.startsWith('00') ? phoneUtil.parse(value) : phoneUtil.parse(value, 'CH');
+    return parsedNumber && !phoneUtil.isValidNumber(parsedNumber) ? { invalidPhone: true } : null;
+  } catch (err) {
+    return {invalidPhone: true};
+  }
+};
+
+export function validateSpace(control: FormControl) {
+  const SPACE_REGEXP = /^[^\s]+(\s+[^\s]+)*$/;
+  return SPACE_REGEXP.test(control.value) ? null : {
+    validateSpace: {
+      valid: false
+    }
+  };
+}
+
+export function PasswordValidator(control: AbstractControl): void {
+  const password = control.get('password');
+  const confirmPassword = control.get('confirm_password');
+  if (password.pristine || confirmPassword.pristine) {
+    confirmPassword.setErrors( null);
+  }
+  if (password && confirmPassword && password.value !== confirmPassword.value) {
+    confirmPassword.setErrors({misMatch: true});
+  }
+}
---
+++ libs/feature/auth/src/lib/models/auth.model.ts
@@ -0,0 +1,57 @@
+export class User {
+  id?: number;
+  first_name: string;
+  last_name: string;
+  email: string;
+  phone: string;
+  password: string;
+  confirm_password: string;
+  gender: string;
+  birth_date: Date;
+  address: Address;
+
+  constructor() {
+    this.first_name = undefined;
+    this.last_name = undefined;
+    this.email = undefined;
+    this.phone = undefined;
+    this.password = undefined;
+    this.confirm_password = undefined;
+    this.address = undefined;
+  }
+}
+
+export class Address {
+  street: string;
+  opt_address_one: string;
+  opt_address_two: string;
+  city: string;
+  district: string;
+
+  constructor() {
+    this.street = undefined;
+    this.opt_address_one = undefined;
+    this.opt_address_two = undefined;
+    this.city = undefined;
+    this.district = undefined;
+  }
+}
+
+export class UserLogin {
+  email: string;
+  password: string;
+
+  constructor() {
+    this.email = undefined;
+    this.password = undefined;
+  }
+
+}
+
+export class ForgotPassword {
+  email: string;
+
+  constructor() {
+    this.email = undefined;
+  }
+}
